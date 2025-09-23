a584ebee9e24db3a93dc6258a4b445b62b7d3efd
icygit6 <182600766+icygit6@users.noreply.github.com>
2025-09-23 22:01:56 +0800

    Update 1_1_Cycle_Detection.cpp

--- 1_1_Cycle_Detection.cpp
+++ 1_1_Cycle_Detection.cpp
@@ -6,7 +6,6 @@ using namespace std;
 const int MAXN = 100005;
 vector<int> adj[MAXN];
 int parent[MAXN];
-int dist[MAXN];
 bool visit[MAXN];
 
 int main() 
@@ -14,7 +13,7 @@ int main()
     int n, m;
     cin >> n >> m;
 
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < m; i++) 
     {
         int x, y;
         cin >> x >> y;
@@ -27,17 +26,18 @@ int main()
         cout << "There is no cycle." << endl;
         return 0;
     }
+
     queue<int> q;
     q.push(1);
     visit[1] = true;
     parent[1] = -1;
-    dist[1] = 0;
 
-    int cycleLength = -1;
+    int extraU = -1, extraV = -1;
 
-    while (!q.empty() && cycleLength == -1) 
+    while (!q.empty() && extraU == -1) 
     {
-        int u = q.front(); q.pop();
+        int u = q.front(); 
+        q.pop();
 
         for (int v : adj[u]) 
         {
@@ -45,24 +45,51 @@ int main()
             {
                 visit[v] = true;
                 parent[v] = u;
-                dist[v] = dist[u] + 1;
                 q.push(v);
             } 
             else if (v != parent[u]) 
             {
-                cycleLength = dist[u] + dist[v] + 1;
+                extraU = u;
+                extraV = v;
                 break;
             }
         }
     }
 
-    if (cycleLength == -1) 
+    if (extraU == -1) 
     {
         cout << "There is no cycle." << endl;
-    } 
-    else 
-    {
-        cout << cycleLength << endl;
+        return 0;
     }
+
+    // backtrack paths from extraU and extraV until they meet
+    vector<bool> mark(n + 1, false);
+    int a = extraU;
+    while (a != -1) 
+    {
+        mark[a] = true;
+        a = parent[a];
+    }
+
+    int cycle = 0;
+    int b = extraV;
+    while (!mark[b]) 
+    {
+        cycle++;
+        b = parent[b];
+    }
+
+    int meet = b;
+    a = extraU;
+    while (a != meet) 
+    {
+        cycle++;
+        a = parent[a];
+    }
+
+    cycle++; // add the extra edge
+
+    cout << cycle << endl;
+
     return 0;
 }
